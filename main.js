e933339626a9e6dc9f9178fc2dcf2a5a2e2c2aef
Emily <lalonde0902@gmail.com>
2019-05-21 15:06:52 -0600

    Add event listener to save button

--- main.js
+++ main.js
@@ -8,34 +8,28 @@ var searchBtn = document.querySelector('.search-btn');
 var searchInput = document.querySelector('.search-input');
 var cardField = document.querySelector('.card-field');
 
+saveBtn.addEventListener('click', createCard);
 
 
 
 
-
-
-
-
-
-
-
-
-function createCard () {
+function createCard (e) {
+e.preventDefault();
 var newCard =
 `<article class="card">
-	<header>
-		<input src="images/star.svg" class="card-icon" id="star-btn"/>
-		<input src="images/delete.svg" class="card-icon" id="delete-btn"/>
-	</header>
-		<section>
-		<h2>Idea title</h2>
-		<p> blah blah blah blah </p>
-	</section>
-		<footer>
-			<input src="images/upvote.svg" class="card-icon" id="upvote-btn"/>
-			<h4>Quality:Swill</h4>
-			<input src="images/downvote.svg" class="card-icon" id="downvote-btn"/>
-		</footer>
+<header>
+  <input type="image" src="images/star.svg" class="card-icon" id="star-btn"/>
+  <input type="image" src="images/delete.svg" class="card-icon" id="delete-btn"/>
+</header>
+  <section>
+  <h2>Idea title</h2>
+  <p> blah blah blah blah </p>
+</section>
+  <footer>
+    <input type="image" src="images/upvote.svg" class="card-icon" id="upvote-btn"/>
+    <h4>Quality:Swill</h4>
+    <input type="image" src="images/downvote.svg" class="card-icon" id="downvote-btn"/>
+  </footer>
 </article>`
 cardField.insertAdjacentHTML('afterbegin', newCard);
 }
