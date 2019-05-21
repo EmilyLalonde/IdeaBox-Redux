fef58e8de7d5851108c15cbd148a8d2b919dee04
Emily <lalonde0902@gmail.com>
2019-05-21 14:25:53 -0600

    Add class constructor in the idea.js file

---
+++ idea.js
@@ -0,0 +1,10 @@
+class Idea {
+  constructor(title, body){
+    this.id = date.now();
+    this.title = title;
+    this.body = body;
+    this.star = false;
+    this.quality = ['Swill', 'Plausible', 'Genius'];
+  }
+}
+
