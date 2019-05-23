237ec6e22ee18a602e897af389a7b02bae4c08f6
Emily <lalonde0902@gmail.com>
2019-05-23 08:59:28 -0600

    Fix constructor issue

--- idea.js
+++ idea.js
@@ -1,4 +1,11 @@
-
+class Idea {
+  constructor(id, title, body){
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.star = false;
+    this.quality = ['Swill', 'Plausible', 'Genius'];
+  };
   saveToStorage (){
     var stringifiedSavedCard = JSON.stringify(storageArray);
     localStorage.setItem('ideas', stringifiedSavedCard);
